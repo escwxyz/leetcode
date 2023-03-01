47e59a8e65afb85da137dee226d136ac7e66895f
Jie <linsijia628@gmail.com>
2023-03-01 15:02:16 +0800

    feat(leetcode:ts): add 832 flipping an image

---
+++ leetcode/typescript/src/832-flipping-an-image.ts
@@ -0,0 +1,34 @@
+/**
+ * @param image - 2D array representing image
+ * @returns 2D array of modified image
+ * Problem:
+ * https://leetcode.cn/problems/flipping-an-image/
+ */
+export const flipAndInvertImage = (image: number[][]): number[][] => {
+	for (let i = 0; i < image.length; i++) {
+		image[i].reverse()
+		for (let j = 0; j < image[i].length; j++) {
+			image[i][j] = image[i][j] === 0 ? 1 : 0
+		}
+	}
+
+	return image
+}
+
+if (import.meta.vitest) {
+	const { it, expect } = import.meta.vitest
+
+	it('test', () => {
+		expect(
+			flipAndInvertImage([
+				[1, 1, 0],
+				[1, 0, 1],
+				[0, 0, 0],
+			])
+		).toStrictEqual([
+			[1, 0, 0],
+			[0, 1, 0],
+			[1, 1, 1],
+		])
+	})
+}
