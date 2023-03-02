728329874ac8635aff52f25d3934fa5f7e58c0f7
Jie <linsijia628@gmail.com>
2023-03-02 20:26:22 +0800

    feat(leetcode:ts): add 1 two sum

---
+++ leetcode/typescript/src/1-two-sum.ts
@@ -0,0 +1,19 @@
+/**
+ * @param nums - Array of integers
+ * @param target - Target number
+ * @returns Indice of two numbers whose sum is equal to target number
+ *
+ * https://leetcode.com/problems/two-sum/
+ */
+export const twoSum = (nums: number[], target: number): number[] => {
+	const arr = []
+	const map = new Map()
+	for (let i = 0; i < nums.length; i++) {
+		const d = target - nums[i]
+		if (map.has(d)) {
+			arr.push(i, map.get(d))
+		}
+		map.set(nums[i], i)
+	}
+	return arr
+}
