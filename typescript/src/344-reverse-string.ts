712f7bd6db8fcdf65fd30f8a587963384c1886b5
Jie <linsijia628@gmail.com>
2023-05-10 10:54:05 +0800

    Add test for 344

--- typescript/src/344-reverse-string.ts
+++ typescript/src/344-reverse-string.ts
@@ -5,12 +5,32 @@
  * https://leetcode.com/problems/reverse-string/
  */
 export const reverseString = (s: string[]): void => {
-	let head: number = 0
-	let tail: number = s.length - 1
+  let head: number = 0
+  let tail: number = s.length - 1
 
-	while (head < tail) {
-		const tmp = s[head]
-		s[head++] = s[tail]
-		s[tail--] = tmp
-	}
+  while (head < tail) {
+    const tmp = s[head]
+    s[head++] = s[tail]
+    s[tail--] = tmp
+  }
+}
+
+if (import.meta.vitest) {
+  const { test, expect } = import.meta.vitest
+
+  test('one', () => {
+    let str = ['h', 'e', 'l', 'l', 'o']
+
+    reverseString(str)
+
+    expect(str).toStrictEqual(['o', 'l', 'l', 'e', 'h'])
+  })
+
+  test('two', () => {
+    let str = ['H', 'a', 'n', 'n', 'a', 'h']
+
+    reverseString(str)
+
+    expect(str).toStrictEqual(['h', 'a', 'n', 'n', 'a', 'H'])
+  })
 }
